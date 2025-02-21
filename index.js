d02d43eabc48b071775801b0ea4789a592662159
Juliano Campos dos Santos <juliano.kampus@gmail.com>
2025-02-21 08:19:41 -0300

    subindo projeto

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import autenticar from "./seguranca/autenticar.js";
 import session from "express-session";
 
 const porta = 3000
-const localhost = '0.0.0.0' //define que nosso aplicativo estará disponível em todas as interfaces de redes desse computador
+const localhost = '0.0.0.0'
 
 const app = express();
 
-// configurar o express para reconhecer o corpo da requisição o campos do formulário
-
-app.use(express.urlencoded({ extended: true })) //biblioteca QS / QueryString
+app.use(express.urlencoded({ extended: true })) 
 
 app.use(session({
     secret: 'chaveSecreta',
@@ -20,17 +18,6 @@ app.use(session({
     }
 }))
 
-// o http é um protocolo estateles (sem estabelecimento de sessão)
-// o servidor recebe uma requisição , processa a requisição e envia uma resposta 
-// sem se preucupar em identificar os atores envolvidos
-
-//com o auxílio da biblioteca express-session 
-//vamos  implementar a halilidade de estabelecer uma sessão 
-// para  um determinado usuário 
-
-
-// oferecer o recurso login
-
 app.get("/login", (requisicao, resposta) => {
     resposta.redirect('/login.html');
 })
@@ -38,29 +25,24 @@ app.get("/login", (requisicao, resposta) => {
 app.post("/login", (requisicao, resposta) => {
     const usuario = requisicao.body.nome
     const senha = requisicao.body.senha
-    if (usuario === 'admin@gmail' && senha === '123') {
+    if (usuario === 'admin' && senha === '123') {
         requisicao.session.autenticado = true;
-        resposta.redirect('/menu.html');
+        resposta.redirect('/login.html?sucesso=1');
     } else {
-        resposta.redirect('/login.html');
+        resposta.redirect('/login.html?erro=1');
     }
 })
-
 
 app.get("/logout", (requisicao, resposta) => {
     requisicao.session.destroy()
     resposta.redirect('/login.html');
 })
 
-//roda o arquivo index.html padrão para a aplicação pasta pública
 app.use(express.static('./public'))
 
-//disponibilizando a estrutura da pasta privada
-// a função autenticar se comporta como um middware aquele que atua na camada do meio 
+
 app.use(autenticar, express.static('./private'))
 
-
-//preparação do servidor para disponibilizar conteúdos estáticos
 app.listen(porta, localhost, () => {
     console.log(`Servidor disponível em ${porta}!`)
 })
--- private/assets/buscar.js
+++ private/assets/buscar.js
@@ -1,50 +1,75 @@
-let form = document.querySelector('form')
-let nome = document.querySelector('.nome')
-let text_button = document.querySelector('.text_button')
-let load = document.querySelector('.load')
-let buscado = document.querySelector('.buscado')
-let error = document.querySelector('.error')
-
-nome.addEventListener('input', () => {
-  if (nome.value) {
-    error.textContent = ''
-  }
-})
-
-function css(elemento, propriedade) {
-  elemento.style.display = propriedade
+// Buscar e exibir os dados do JSON
+const selector = (element) => {
+  return document.querySelector(element)
 }
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  css(text_button, 'none')
-  css(load, 'block')
-  buscado.textContent = ''
-  error.textContent = ''
-  let buscarNome = (nome) => {
-    return new Promise((resolve, reject) => {
-      if (nome) {
-        setTimeout(() => {
-          css(text_button, 'block')
-          css(load, 'none')
-          resolve(nome)
-        }, 2000);
-      } else {
-        setTimeout(() => {
-          css(text_button, 'block')
-          css(load, 'none')
-          reject('Erro! Por favor digite um nome')
-        }, 2000);
-      }
-    })
-  };
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./assets/dados.json")
+    .then((response) => response.json())
+    .then((data) => {
 
-  (async () => {
-    try {
-      let nome_async = await buscarNome(nome.value)
-      buscado.textContent = nome_async
-    } catch (erro) {
-      error.textContent = erro
-    }
-  })()
-})
+      let id = localStorage.getItem("id")
+
+      const quantidade = document.getElementById('quantidade');
+      document.getElementById('incrementar').addEventListener('click', function () {
+        if (data[id].lugaresDisponiveis != 0) {
+          selector('.lugares').innerHTML = data[id].lugaresDisponiveis -= 1
+          quantidade.value = parseInt(quantidade.value) + 1;
+          selector('.calc').innerHTML = parseFloat(data[id].calc * quantidade.value).toFixed(3)
+        } else {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-center",
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            },
+          });
+          Toast.fire({ icon: "info", title: "Você selecionou todos os lugares disponíveis!" });
+        }
+      });
+
+      document.getElementById('decrementar').addEventListener('click', function () {
+        if (parseInt(quantidade.value) > 0) {
+          selector('.lugares').innerHTML = data[id].lugaresDisponiveis += 1
+          quantidade.value = parseInt(quantidade.value) - 1;
+          selector('.calc').innerHTML = parseFloat(data[id].calc * quantidade.value).toFixed(3)
+        }
+      });
+
+      selector('.nome_pacote').innerHTML = data[id].nome
+      selector('.img_viagem_detalhe').src = data[id].src
+      selector('.dataPartida').innerHTML = data[id].dataPartida
+      selector('.duracao').innerHTML = data[id].duracao
+      selector('.partida').innerHTML = data[id].localPartida
+      selector('.lugares').innerHTML = data[id].lugaresDisponiveis
+      selector('.preco').innerHTML = parseFloat(data[id].preco).toFixed(3)
+
+
+      selector('.transp').innerHTML = data[id].transp
+      selector('.hosp').innerHTML = data[id].hosp
+      selector('.aliment').innerHTML = data[id].aliment
+      selector('.passeios').innerHTML = data[id].passeios
+      selector('.pagament').innerHTML = data[id].pagament
+      selector('.descricao_content').innerHTML = data[id].descricao
+      selector('.frame').src = data[id].frame
+
+    })
+    .catch((error) => console.error("Erro ao carregar JSON:", error));
+});
+
+let buy = selector('.comprar')
+let loader = selector('.loader')
+
+buy.onclick = () => {
+  loader.style.display = 'flex';
+  buy.childNodes[1].textContent = ''
+  setTimeout(() => {
+    loader.style.display = 'none';
+    buy.childNodes[1].textContent = 'Comprar'
+  }, 1000);
+
+}
